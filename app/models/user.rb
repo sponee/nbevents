d7611a930e1a5197fad9f50ebee05263d836ccda
Kyle Sponheim <kyle.sponheim@gmail.com>
2016-07-29 12:50:08 -0700

    readding omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
   def self.from_omniauth(auth)
     user = where(provider: auth.provider, uid: auth.uid).first
