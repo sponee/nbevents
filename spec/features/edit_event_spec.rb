d24a9bbd1a6837259e54e71b400ffa537ec2f7b5
Kyle Sponheim <kyle.sponheim@gmail.com>
2016-07-28 23:40:13 -0700

    more concise update test

--- spec/features/edit_event_spec.rb
+++ spec/features/edit_event_spec.rb
@@ -5,18 +5,15 @@ describe EventsController do
   before(:each) do
     login
     create_nation_api_token
+    create_client
   end
 
   describe "update" do
 
     it "updates and saves the event" do 
 
-      slug = 'nbevents'
-      token = ENV["TESTING_TOKEN"]
-      client = NationBuilder::Client.new(slug, token)
-      event = event_attributes
-      client.call(:events, :create, event)      
-      response = client.call(:events, :update, event)
+      event = event_attributes  
+      response = @client.call(:events, :update, event)
 
       expect(response["status_code"]).to eq(200)
     end
