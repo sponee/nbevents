6971bacf7e63dcce25420266ef9f8850377ee173
Kyle Sponheim <kyle.sponheim@gmail.com>
2016-07-29 12:48:55 -0700

    removing omniauth to test a bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+         :recoverable, :rememberable, :trackable, :validatable
 
   def self.from_omniauth(auth)
     user = where(provider: auth.provider, uid: auth.uid).first
